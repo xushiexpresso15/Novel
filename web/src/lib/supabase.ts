5a49042b6595485b254079b3e1502328d05706fc
XuShiExpress <freud123freud234freud345@gmail.com>
2025-12-20 00:52:14 +0800

    v1-snapshot4-3

--- web/src/lib/supabase.ts
+++ web/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://owehtfxlpgoejooqpmpe.supabase.co'
-const supabaseAnonKey = 'sb_publishable_QcKWFuJxRX4XXi55vLaxWA_A1FficHy'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93ZWh0ZnhscGdvZWpvb3FwbXBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNDgxODQsImV4cCI6MjA4MTcyNDE4NH0.CrG8cObuDsTsBbWDF2jp3ix6v2YPbqpuvP6hbWrHW-Y'
 
 export const supabase = createClient(
     supabaseUrl,
