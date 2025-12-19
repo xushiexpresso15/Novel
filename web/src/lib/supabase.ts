cd177707154ea51121f8eb5bcc54c59de5705328
XuShiExpress <freud123freud234freud345@gmail.com>
2025-12-20 00:24:29 +0800

    v1-snapshot4-1

--- web/src/lib/supabase.ts
+++ web/src/lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Hardcoded for current debugging session
 const supabaseUrl = 'https://owehtfxlpgoejooqpmpe.supabase.co'
 const supabaseAnonKey = 'sb_publishable_QcKWFuJxRX4XXi55vLaxWA_A1FficHy'
 
