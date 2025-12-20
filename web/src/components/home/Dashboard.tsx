6bb000f202f3a3ba51bd35a71d0391c911979a8d
XuShiExpress <freud123freud234freud345@gmail.com>
2025-12-20 23:16:55 +0800

    \v1-snapshot9-13-1\

--- web/src/components/home/Dashboard.tsx
+++ web/src/components/home/Dashboard.tsx
@@ -5,11 +5,5 @@ import { NovelList } from "./NovelList"
 import { EditorLayout } from "@/components/editor/EditorLayout"
 
 export function Dashboard() {
-    const { selectedNovelId } = useNovelStore()
-
-    if (selectedNovelId) {
-        return <EditorLayout />
-    }
-
     return <NovelList />
 }
